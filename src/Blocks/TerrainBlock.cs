f11a15084f03b961803a032787d970fe41902bd2
Humberto Aleman <alemanhumberto06@gmail.com>
2023-11-26 16:20:07 -0400

    Fixed MORE STUFF

--- src/Blocks/TerrainBlock.cs
+++ src/Blocks/TerrainBlock.cs
@@ -1,9 +1,10 @@
+using Amber.Engine;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Amber.Blocks;
 
-public class TerrainBlock : Entity.Entity
+public class TerrainBlock : Entity
 {
     private const int gridSize = 32;
 
