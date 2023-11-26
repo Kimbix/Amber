491077db46404bd77fa856772eb21836f1909379
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2023-11-26 17:42:10 -0400

    Lobotomy

--- src/Engine/Entity.cs
+++ src/Engine/Entity.cs
@@ -4,8 +4,22 @@ namespace Amber.Engine;
 
 public abstract class Entity
 {
+    private const float GRAVITY = 9.8f;
+    private const float MASS = 9.8f;
+    private const float ACCELERATION_CAP = 18.0f;
+
     public Vector2 Position;
     public Vector2 Velocity;
     public Vector2 Acceleration;
     public Hitbox Hitbox = null;
+
+    public void Update()
+    {
+        // Compiler will optimize this don't worry
+        float A = GRAVITY / MASS;
+
+        if (Acceleration.X - A > 0) Acceleration.X -= A; else Acceleration.X = 0;
+        if (Acceleration.Y + A <= ACCELERATION_CAP) Acceleration.Y += A;
+        Position += Acceleration;
+    }
 }
--- src/Player.cs
+++ src/Player.cs
@@ -1,8 +1,52 @@
+using System.Diagnostics.Metrics;
+using Microsoft.Xna.Framework;
 using Amber.Engine;
 
 namespace Amber;
 
 public class Player : Entity
 {
+    private bool Alive = true;
+    private bool Jumping = false;
+    private bool Dashing = false;
     
+    public void Move(Vector2 newPosition)
+    {
+        this.Position = newPosition;
+    }
+    
+    // Same as Move. Hopefully the compiler will optimize this thing
+    public void SetPosition(Vector2 newPosition)
+    {
+        Move(newPosition);
+    }
+    
+    public void SetVelocity(Vector2 newVelocity)
+    {
+        this.Velocity = newVelocity;
+    }
+
+    public void SetAcceleration(Vector2 newAcceleration)
+    {
+        this.Acceleration = newAcceleration;
+    }
+    
+    // Called whenever the player collides with another entity
+    public void OnPlayerCollision(Entity entity)
+    {
+        
+    }
+    
+    // Getter for Alive
+    public bool IsAlive()
+    {
+        return Alive;
+    }
+    
+    // Called to trigger the player's death animation
+    public void Die()
+    {
+        // Play sprite animation for death
+        Alive = false;
+    }
 }
